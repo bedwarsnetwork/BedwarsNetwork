24441ebee0d0370926ee7ff5a28aa33fab10386a
Sebastian Binder <sebastianbinder@users.noreply.github.com>
2017-01-21 15:25:19 +0100

    Restore Ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,47 +2,60 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
+    
     user ||= User.new
-
-    alias_action :index, :search, :show, :to => :chatlog_manage
-    alias_action :index, :update, :online, :chatlogs, :to => :user_manage
-    alias_action :search, :show, :statistic, :youtube, :to => :user_perms
-
-
+    
     if user.has_role? "Admin"
       can :manage, :all
     end
-
+    
     if user.has_role? "SeniorBuilder"
       can :access, :dashboard
-
-      can :chatlog_manage, Chatlog
+      
+      can :index, Chatlog
       can :list, Chatlog
-
-      can :user_manage, User
+      can :search, Chatlog
+      can :show, Chatlog
+      
+      can :index, User
+      can :update, User
+      can :online, User
+      can :chatlogs, User
     end
 
     if user.has_role? "Supporter"
       can :access, :dashboard
-
-      can :chatlog_manage, Chatlog
-
-      can :user_manage, User
+      
+      can :index, Chatlog
+      can :search, Chatlog
+      can :show, Chatlog
+      
+      can :index, User
+      can :update, User
+      can :online, User
+      can :chatlogs, User
     end
-
+    
     if user.has_role? "Moderator"
       can :access, :dashboard
-
-      can :chatlog_manage, Chatlog
-
-      can :user_manage, User
+      
+      can :index, Chatlog
+      can :search, Chatlog
+      can :show, Chatlog
+      
+      can :index, User
+      can :update, User
+      can :online, User
+      can :chatlogs, User
     end
-
-    can :user_perms, User
-
+    
+    can :search, User
+    can :show, User
+    can :statistic, User
+    can :youtube, User
+    
     can :update, User, :id => user._id
-
+    
     can :show, Chatlog do |chatlog|
       access = false
       chatlog.chatlogmessages.each do |message|
