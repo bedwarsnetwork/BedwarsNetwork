0d86407c0378835a16c4ded47c66c3924cc789b5
Sebastian Binder <designstudio@sebastianbinder.com>
2017-03-15 13:39:54 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :repo_url, 'git@github.com:bedwarsnetwork/www.bedwars.network.git'
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
+set :name, "www.bedwars.network-#{fetch(:rails_env)}"
+
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
@@ -42,16 +44,23 @@ namespace :deploy do
   after :updated, :build
   task :build do
     on roles(:app) do
-      execute "cd #{fetch(:release_path)} && echo 'RAILS_ENV=#{fetch(:rails_env)}' > .env"
-      execute "cd #{fetch(:release_path)} && echo 'EXTERNAL_PORT=3012' >> .env"
+      execute "cd #{fetch(:release_path)} && echo 'NAME=#{fetch(:name)}' > .env"
+      execute "cd #{fetch(:release_path)} && echo 'EXTERNAL_PORT=3012' > .env"
       execute "cd #{fetch(:release_path)} && docker-compose build"
     end
   end
-  after :build, :start
+  after :build, :stop
   task :start do
     on roles(:app) do
       # in case the app isn't running on the other end
-      execute "cd #{fetch(:release_path)} && docker-compose up"
+      execute "cd #{fetch(:release_path)} && docker-compose up -d"
+    end
+  end
+  after :stop, :start
+  task :start do
+    on roles(:app) do
+      # in case the app isn't running on the other end
+      execute "cd #{fetch(:release_path)} && docker-compose up -d"
     end
   end
 end
