24c41698a9981aa8f5312e8f1ed9a0f43bdab814
Sebastian Binder <designstudio@sebastianbinder.com>
2017-07-28 14:04:18 +0200

    Latest

--- app/controllers/dashboard/serverstatistics_controller.rb
+++ app/controllers/dashboard/serverstatistics_controller.rb
@@ -8,30 +8,33 @@ class Dashboard::ServerstatisticsController < ApplicationController
   end
   
   def search
-    if params[:search].empty? || (params[:search].to_s.length < 3 && !(can? :index, User))
+    if params[:search].empty? || (params[:search].to_s.length < 3 && !(can? :index, Serverstatistic))
       redirect_back(fallback_location: home_path, :flash => { :error => "Suchbegriff zu kurz" })
     elsif params[:search] && request.post?
-      redirect_to search_result_dashboard_users_path(params[:search])
+      redirect_to search_result_dashboard_serverstatistics_path(params[:search])
     elsif params[:search] && request.get?
       @users = User.where({
         "$or" => [
-          {_id: "#{params[:search]}" },
-          {name: /.*#{params[:search]}.*/i }
+          {_id: "#{params[:search]}" }
         ]
       }).order_by(:name => 'asc').page params[:page]
-      if @users.count == 1
-        redirect_to dashboard_user_path(@users.first)
+      if @serverstatistics.count == 1
+        redirect_to dashboard_serverstatistics_path(@serverstatistics.first)
       else
-        @page_title = ["Spieler", "Suche", params[:search]]
+        @page_title = ["Server-Statistik", "Suche", params[:search]]
         render 'index'
       end
 		end
 	end
   	
 	def show
-      @serverstatistic = Serverstatistic.find_by(id: params[:id])
+  	  if(params[:id] == "latest")
+    	 @serverstatistic = Serverstatistic.find_by(id: DateTime.now.in_time_zone("Berlin").strftime("%Y-%m-%d"))
+      else
+        @serverstatistic = Serverstatistic.find_by(id: params[:id])
+      end
     if @serverstatistic.nil?
-      redirect_back(fallback_location: home_path, :flash => { :error => "Spieler nicht gefunden" })
+      redirect_back(fallback_location: home_path, :flash => { :error => "Statistik nicht gefunden" })
     end
     @page_title = ["Server-Statistik", @serverstatistic._id.in_time_zone("Berlin").strftime("%A, %d.%m.%Y")]
 	end
